3bb8baa7de9cfec8b418b04af5959facfeec1d2c
Hridansh <73342299+Hridansh@users.noreply.github.com>
2021-10-20 16:42:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480, 280);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+
+function modelloaded(){
+    console.log("Model Loaded!!!")
+}
